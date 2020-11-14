c1af79b508e42217daf88f50da918f50d7a44d7e
Ori Petel <oripetel1306@gmail.com>
2020-11-14 18:32:26 +0200

    Another try...

--- ex1/pt.c
+++ ex1/pt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "os.h"
 
-#define PT_LEVELS 5 // 5 levels of PTE
+// 5 levels of PTE
+#define PT_LEVELS 5
 #define SYMBOL_BITS 9
 #define OFFSET_BITS 12
 #define PTE_BYTES 8
@@ -9,28 +10,41 @@
 const uint64_t SYMBOL_MASK = 0x1FF; // mask of 9 lower bits
 const uint64_t VALID_MASK = 0x1;    // mask of the LSB
 
+enum walk_mode
+{
+    Search = 0,
+    Destroy = 0,
+    Insert = 1
+};
+
 /**
  * Returns whether a pte (= page table entry) is valid.
  */
-inline uint64_t is_valid_pte(uint64_t *pte_ptr)
+inline uint64_t is_valid_pte(uint64_t pte)
 {
-    return *pte_ptr && VALID_MASK;
+    return pte && VALID_MASK;
 }
 
 /**
  * Creates a pte (= page table entry) structure out of a page frame number.
  */
-inline uint64_t create_pte(uint64_t frame)
+inline uint64_t create_pte(uint64_t frame_number)
 {
-    return (frame << OFFSET_BITS) || VALID_MASK;
+    return (frame_number << OFFSET_BITS) + VALID_MASK;
 }
 
 /**
  * Gets the page number out of a pte (= page table entry).
  */
-inline uint64_t get_page_number(uint64_t *pte_ptr)
+inline uint64_t get_frame_number(uint64_t pte)
 {
-    return *pte_ptr >> OFFSET_BITS;
+    return pte >> OFFSET_BITS;
+}
+
+int get_index(uint64_t vpn, int level)
+{
+    int offset = SYMBOL_BITS * (PT_LEVELS - (level + 1));
+    return (vpn >> offset) && SYMBOL_MASK;
 }
 
 // /**
@@ -45,32 +59,32 @@ inline uint64_t get_page_number(uint64_t *pte_ptr)
  * Returns a pointer to a Page Table leaf (represents the actual mapping entry of a vpn to a ppn)
  * If there is no such mapping, returns NULL
  */
-uint64_t *page_walk(uint64_t pt, uint64_t vpn, int insert_flag)
+uint64_t *page_walk(uint64_t pt, uint64_t vpn, enum walk_mode mode)
 {
+    int index;
+    uint64_t *node;
     uint64_t *pte_ptr;
-    uint64_t index;
-
-    unsigned int mask_shift = SYMBOL_BITS * (PT_LEVELS - 1);
 
     for (int i = 0; i < PT_LEVELS; i++)
     {
         // Obtain the symbol from the vpn at each pt level, and slide the "mask window" to the right.
-        index = (vpn && (SYMBOL_MASK << mask_shift)) >> mask_shift;
-        mask_shift -= SYMBOL_BITS;
+        index = get_index(vpn, i);
 
-        // Get the page table entry
-        pte_ptr = (uint64_t *)phys_to_virt(pt << OFFSET_BITS);
-        pte_ptr += index;
+        // Get the page table node
+        node = (uint64_t *)phys_to_virt(pt << OFFSET_BITS);
+
+        // Get the page table entry pointer
+        pte_ptr = &(node[index]);
 
         // Check for mapping
-        if (!is_valid_pte(pte_ptr))
+        if (!is_valid_pte(*pte_ptr))
         {
-            if (insert_flag)
+            if (mode == Insert)
             {
                 // Prevent from allocating new frame the last level
                 if (i == PT_LEVELS - 1)
                 {
-                    break;
+                    return pte_ptr;
                 }
 
                 // Create a new pte
@@ -84,7 +98,7 @@ uint64_t *page_walk(uint64_t pt, uint64_t vpn, int insert_flag)
         }
 
         // Update the pt to one level ahead
-        pt = get_page_number(pte_ptr);
+        pt = get_frame_number(*pte_ptr);
     }
     return pte_ptr; // Returns a pte leaf that represents the actual mapping of the vpn
 }
@@ -96,26 +110,26 @@ void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn)
     if (ppn == NO_MAPPING)
     {
         // Destroy vpn mapping
-        pte_leaf_ptr = page_walk(pt, vpn, 0);
+        pte_leaf_ptr = page_walk(pt, vpn, Destroy);
         if (pte_leaf_ptr != NULL)
         {
-            *pte_leaf_ptr = 0;
+            *pte_leaf_ptr = 0ULL;
         }
     }
     else
     {
         // Set vpn mapping to ppn
-        pte_leaf_ptr = page_walk(pt, vpn, 1);
+        pte_leaf_ptr = page_walk(pt, vpn, Insert);
         *pte_leaf_ptr = create_pte(ppn);
     }
 }
 
 uint64_t page_table_query(uint64_t pt, uint64_t vpn)
 {
-    uint64_t *pte_leaf_ptr = page_walk(pt, vpn, 0);
+    uint64_t *pte_leaf_ptr = page_walk(pt, vpn, Search);
     if (pte_leaf_ptr != NULL)
     {
-        return get_page_number(pte_leaf_ptr);
+        return get_frame_number(*pte_leaf_ptr);
     }
     return NO_MAPPING;
 }
