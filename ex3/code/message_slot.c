76224b8f88ce210f1ca84e26375008c89eab9351
Ori Petel <oripetel1306@gmail.com>
2020-12-19 11:54:40 +0200

    Finished implementing the kernel module

--- ex3/code/message_slot.c
+++ ex3/code/message_slot.c
@@ -7,68 +7,261 @@
 #undef MODULE
 #define MODULE
 
-#include <linux/fs.h>      /* for register_chrdev */
-#include <linux/kernel.h>  /* We're doing kernel work */
-#include <linux/module.h>  /* Specifically, a module */
+#include <linux/fs.h>     /* for register_chrdev */
+#include <linux/kernel.h> /* We're doing kernel work */
+#include <linux/module.h> /* Specifically, a module */
+#include <linux/slab.h>
 #include <linux/string.h>  /* for memset. NOTE - not string.h!*/
 #include <linux/uaccess.h> /* for get_user and put_user */
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ori Petel");
 
 // Our custom definitions of IOCTL operations
 #include "message_slot.h"
 
-//================== DEVICE FUNCTIONS ===========================
-static int device_open(struct inode *inode, struct file *file)
+// ==================== Debug Functions ========================
+static unsigned int info_counter = 0;
+
+unsigned int get_info_counter(void)
 {
+    return ++info_counter;
+}
+
+#define INFO(message, ...) printk(KERN_DEBUG "\n\nMessage_Slot-%d: " message "\n\n", get_info_counter(), ##__VA_ARGS__);
+
+// ================= Define file handlers =======================
+static int const UINT = sizeof(unsigned int);
+
+/**
+ * Release the minor and the channel from the files
+ */
+void get_detais(const struct file *_file, unsigned int *minor, unsigned int *channel_id)
+{
+    memcpy(minor, _file->private_data, UINT);
+    memcpy(channel_id, _file->private_data + UINT, UINT);
+}
+
+/**
+ * Set the minor to the file
+ */
+void set_minor(struct file *_file, const unsigned int minor)
+{
+    memcpy(_file->private_data, &minor, UINT);
+}
+
+/**
+ * Set the channel id to the file
+ */
+void set_channel_id(struct file *_file, const unsigned int channel_id)
+{
+    memcpy(_file->private_data + UINT, &channel_id, UINT);
+}
+
+// -================ Define database structures =================
+
+// Using Linux kernel lists
+typedef struct
+{
+    unsigned int id;
+    unsigned int message_size;
+    char message[BUF_LEN];
+
+    struct list_head list_node;
+
+} channel_t;
+
+static struct list_head slots[SLOT_AMOUNT];
+
+/**
+ * Get the channel configured to the file
+ */
+channel_t *get_channel(unsigned int minor, unsigned int id)
+{
+
+    struct list_head *slot;
+    channel_t *channel;
+
+    slot = &slots[minor];
+
+    // Find the required channel
+    list_for_each_entry(channel, slot, list_node)
+    {
+        if (channel->id == id)
+        {
+            return channel;
+        }
+    }
+
+    // In case not found
+    return NULL;
+}
+
+/**
+ * Add the channel configured to the file
+ */
+int add_channel(unsigned int minor, unsigned int id)
+{
+    struct list_head *slot;
+    channel_t *channel;
+
+    slot = &slots[minor];
+
+    // Allocate a new channel
+    channel = (channel_t *)kmalloc(sizeof(channel_t), GFP_KERNEL);
+
+    // If allocation failed
+    if (channel == NULL)
+    {
+        return -EHWPOISON;
+    }
+
+    // Add the channel to the slot
+    channel->id = id;
+    list_add_tail(&channel->list_node, slot);
     return SUCCESS;
 }
 
-//---------------------------------------------------------------
-static int device_release(struct inode *inode, struct file *file)
+/**
+ * Free all the memory took by the slots
+ */
+void free_slots(void)
 {
-    printk("Invoking device_release(%p,%p)\n", inode, file);
+    int i;
+    struct list_head *slot;
+    channel_t *the_channel;
+    channel_t *temp_channel;
+
+    for (i = 0; i < SLOT_AMOUNT; i++)
+    {
+        slot = &slot[i];
+
+        list_for_each_entry_safe(the_channel, temp_channel, slot, list_node)
+        {
+
+            // Remove the channel from the list
+            list_del(&the_channel->list_node);
+
+            // Free the memory
+            kfree(the_channel);
+        }
+    }
+}
+
+//================== DEVICE FUNCTIONS ===========================
+static int device_open(struct inode *inode, struct file *_file)
+{
+    printk("Invoking device_open(%p,%p)\n", inode, _file);
+
+    set_minor(_file, iminor(inode));
+    set_channel_id(_file, 0);
+
     return SUCCESS;
+}
+
+//----------------------------------------------------------------
+static long device_ioctl(struct file *_file, unsigned int ioctl_command_id, unsigned long ioctl_param)
+{
+    // Switch according to the ioctl called
+    if (MSG_SLOT_CHANNEL == ioctl_command_id)
+    {
+        unsigned int f_minor, f_id;
+        unsigned int id = (unsigned int)ioctl_param;
+
+        // Get the parameter given to ioctl by the process
+        printk("Invoking ioctl: setting encryption "
+               "flag to %ld\n",
+               ioctl_param);
+
+        if (id == 0)
+        {
+            return -EINVAL;
+        }
+        set_channel_id(_file, id);
+
+        // Get minor and channel id
+        get_detais(_file, &f_minor, &f_id);
+
+        // Check if the channel already exists
+        if (get_channel(f_minor, f_id) == NULL)
+        {
+            return add_channel(f_minor, f_id);
+        }
+
+        return SUCCESS;
+    }
+    return -EINVAL;
 }
 
 //---------------------------------------------------------------
 // a process which has already opened
 // the device file attempts to read from it
-static ssize_t device_read(struct file *file, char __user *buffer, size_t length, loff_t *offset)
+static ssize_t device_read(struct file *_file, char __user *buffer, size_t length, loff_t *offset)
 {
-    return SUCCESS;
+    unsigned int f_minor, f_id;
+    channel_t *channel;
+
+    printk("Invoking device_read(%p,%ld)\n", _file, length);
+
+    // Get minor and channel id
+    get_detais(_file, &f_minor, &f_id);
+
+    if (f_id == 0)
+    {
+        return -EINVAL;
+    }
+
+    // Get the required channel
+    channel = get_channel(f_minor, f_id);
+
+    if (channel->message_size == 0)
+    {
+        return -EWOULDBLOCK;
+    }
+
+    if (length < channel->message_size)
+    {
+        return -ENOSPC;
+    }
+
+    if (copy_to_user(buffer, channel->message, channel->message_size))
+    {
+        return -EINVAL;
+    }
+
+    return channel->message_size;
 }
 
 //---------------------------------------------------------------
 // a processs which has already opened
 // the device file attempts to write to it
-static ssize_t device_write(struct file *file, const char __user *buffer, size_t length, loff_t *offset)
+static ssize_t device_write(struct file *_file, const char __user *buffer, size_t length, loff_t *offset)
 {
-    int i;
-    char the_message[100] = "Hello World!";
-    printk("Invoking device_write(%p,%ld)\n", file, length);
-    for (i = 0; i < length && i < BUF_LEN; ++i)
+    unsigned int f_minor, f_id;
+    channel_t *channel;
+
+    printk("Invoking device_write(%p,%ld)\n", _file, length);
+
+    // Get minor and channel id
+    get_detais(_file, &f_minor, &f_id);
+
+    if (f_id == 0)
     {
-        get_user(the_message[i], &buffer[i]);
+        return -EINVAL;
     }
 
-    // return the number of input characters used
-    return i;
-}
-
-//----------------------------------------------------------------
-static long device_ioctl(struct file *file, unsigned int ioctl_command_id, unsigned long ioctl_param)
-{
-    // Switch according to the ioctl called
-    if (MSG_SLOT_CHANNEL == ioctl_command_id)
+    if (length <= 0 || length > 128)
     {
-        // Get the parameter given to ioctl by the process
-        printk("Invoking ioctl: setting encryption "
-               "flag to %ld\n",
-               ioctl_param);
+        return -EMSGSIZE;
     }
 
-    return SUCCESS;
+    if (copy_from_user(channel->message, buffer, length))
+    {
+        return -EINVAL;
+    }
+
+    channel->message_size = length;
+    return length;
 }
 
 //==================== DEVICE SETUP =============================
@@ -81,7 +274,6 @@ struct file_operations fops = {
     .write = device_write,
     .open = device_open,
     .unlocked_ioctl = device_ioctl,
-    .release = device_release,
 };
 
 //---------------------------------------------------------------
@@ -95,7 +287,7 @@ static int __init simple_init(void)
     // Negative values signify an error
     if (rc < 0)
     {
-        printk(KERN_ALERT "%s registraion failed for  %d\n", DEVICE_RANGE_NAME, MAJOR_NUM);
+        printk(KERN_ERR "%s registraion failed for  %d\n", DEVICE_RANGE_NAME, MAJOR_NUM);
         return rc;
     }
 
--- ex3/code/message_slot.h
+++ ex3/code/message_slot.h
@@ -4,10 +4,6 @@
 #include <linux/ioctl.h>
 
 // The major device number.
-// We don't rely on dynamic registration
-// any more. We want ioctls to know this
-// number at compile time.
-//#define MAJOR_NUM 244
 #define MAJOR_NUM 240
 
 // Set the message of the device driver
